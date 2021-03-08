38d7c012ee7bcc6dcc233d4a7c1c48d6dd941e62
PalomiitaaG <palomagutierrez947@gmail.com>
2021-03-08 18:55:59 +0100

    examen

---
+++ Examen 2 eval_3/javascript.js
@@ -0,0 +1,3 @@
+var color=["red","blue","black"];
+var image=["ORCA_AREO_M21eTEAM.jpg","ORCA_AREO_M21eTEAMA.jpg","ORCA_AREO_M21eTEAMB.jpg"];
+document.getElementById('img_grand').src="img"+image[0];
